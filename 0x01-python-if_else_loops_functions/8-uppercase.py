820d3c84b94bffba96f31de515e96af7a6d6b83f
Sugarbaby04 <fakodam04@gmail.com>
2022-12-01 14:53:28 -0800

    ghj

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,15 @@
- #!/usr/bin/python3
+
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    result = ''
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{:s}".format(result)) #!/usr/bin/python3
 def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
